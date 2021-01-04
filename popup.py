337a058cebc08fbd3e9fd7f50c5a9b41f93a0bfe
John Elder <john@codemy.com>
2021-01-04 08:22:25 -0800

    popup boxes

---
+++ popup.py
@@ -0,0 +1,19 @@
+from kivy.app import App
+from kivy.uix.widget import Widget
+from kivy.lang import Builder
+
+# Designate Our .kv design file 
+Builder.load_file('popup.kv')
+
+class MyLayout(Widget):
+	pass	
+
+class AwesomeApp(App):
+	def build(self):
+		return MyLayout()
+
+if __name__ == '__main__':
+	AwesomeApp().run()
+
+
+
