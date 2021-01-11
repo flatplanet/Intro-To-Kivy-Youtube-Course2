e3546a5f5c0e8c63cf7f42762c4523532429ff46
John Elder <john@codemy.com>
2021-01-11 08:44:07 -0800

    splitter

---
+++ split.py
@@ -0,0 +1,22 @@
+from kivy.app import App
+from kivy.uix.widget import Widget
+from kivy.properties import ObjectProperty
+from kivy.lang import Builder
+
+
+# Designate Our .kv design file 
+Builder.load_file('split.kv')
+
+class MyLayout(Widget):
+    pass
+
+class AwesomeApp(App):
+    def build(self):
+        return MyLayout()
+
+if __name__ == '__main__':
+    AwesomeApp().run()
+
+
+
+
